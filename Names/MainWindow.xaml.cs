09a32a95a9e83f479f5a2b2a9a60de18264f55cb
Chaz Hall <chaz.h.hall@gmail.com>
2022-01-27 22:33:12 -0700

    Add project files.

---
+++ Names/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Names
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //method checks if the entry is null or blank or is lstNames already contains the entry
+        //else it adds the new entry and then clears teh txtName entry box
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+                if(!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
+                lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
+        }
+    }
+}
